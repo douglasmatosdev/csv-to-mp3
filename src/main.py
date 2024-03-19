39d636af1208a71ed73c47b9df800c66f006cb5f
douglasmatosdev <douglasmatosdev@gmail.com>
2024-03-19 11:56:23 -0300

    add regex

--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
 from gtts import gTTS
+import re
 import os
 import csv
 
-with open('english-sentences.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+FILE_NAME_SIZE = 30
+
+os.system('rm -r ./audios/*')
+
+with open("english-sentences.csv", newline="") as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=" ", quotechar="|")
     for i, row in enumerate(spamreader):
-        text = ' '.join(row)
-        
-        file_name = text.lower().replace(' ', '_').replace(',', '').replace('.', '').replace('\'', '').replace('"', '').replace('-', '')
-        
+        text = " ".join(row)
+
+        file_name = text[:FILE_NAME_SIZE].strip().lower()
+        file_name = re.sub(r"(\,|\.|\"|\'|\-|\_)", "", file_name)
+        file_name = file_name.replace(" ", "_")
+
         audio = gTTS(text=text, lang="en", slow=False)
-        audio.save(f'audios/{file_name}.mp3')
+        audio.save(f"audios/{file_name}.mp3")
