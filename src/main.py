8a9bce1001c0d9dd0eeca453bab1810a61e5877b
douglasmatosdev <douglasmatosdev@gmail.com>
2024-03-19 18:16:22 -0300

    DicReader

--- src/main.py
+++ src/main.py
@@ -8,9 +8,10 @@ FILE_NAME_SIZE = 30
 os.system('rm -r ./audios/*')
 
 with open("english-sentences.csv", newline="") as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=" ", quotechar="|")
-    for i, row in enumerate(spamreader):
-        text = " ".join(row)
+    file = csv.DictReader(csvfile)
+    for col in file:
+        text = col['English']
+        print(col)
 
         file_name = text[:FILE_NAME_SIZE].strip().lower()
         file_name = re.sub(r"(\,|\.|\"|\'|\-|\_)", "", file_name)
