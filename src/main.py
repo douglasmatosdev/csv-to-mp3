fa1d862b1ad3a67b7cc8f73b83522b058fc400ca
douglasmatosdev <douglasmatosdev@gmail.com>
2024-03-20 19:41:10 -0300

    update

--- src/main.py
+++ src/main.py
@@ -5,17 +5,20 @@ import csv
 
 FILE_NAME_SIZE = 30
 
-os.system('rm -r ./audios/*')
+COLUMN_EN = "English"
 
-with open("english-sentences.csv", newline="") as csvfile:
+INPUT_FILE = "input.csv"
+
+os.system("rm -rf ./audios/*")
+
+with open(INPUT_FILE, newline="") as csvfile:
     file = csv.DictReader(csvfile)
     for col in file:
-        text = col['English']
-        print(col)
+        text_en = col[COLUMN_EN]
 
-        file_name = text[:FILE_NAME_SIZE].strip().lower()
+        file_name = text_en[:FILE_NAME_SIZE].strip().lower()
         file_name = re.sub(r"(\,|\.|\"|\'|\-|\_)", "", file_name)
         file_name = file_name.replace(" ", "_")
 
-        audio = gTTS(text=text, lang="en", slow=False)
+        audio = gTTS(text=text_en, lang="en", slow=False)
         audio.save(f"audios/{file_name}.mp3")
